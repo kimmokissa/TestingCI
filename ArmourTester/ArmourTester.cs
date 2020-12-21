3ebca56a853011fd0b252f53e728f6becdde833c
kimmokissa <60046096+kimmokissa@users.noreply.github.com>
2020-12-21 23:04:26 +0200

    testprojekti päivitys

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void TestLevel1()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int lvl = ar.getLevel();
+
+            if (lvl != 2) Assert.Fail();
+
+        }
+        [TestMethod]
+        public void TestSlot1()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int slot = ar.getSlot();
+
+            if (slot != 2) Assert.Fail();
+
+        }
+    }
+}
